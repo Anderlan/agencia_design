6273187c8148aaea86b54da1f48fa9546ecaa742
anderlan <anderlansiqueira@gmail.com>
2023-06-30 16:49:25 -0300

    Atualizado

--- src/componentes/secaoExperienciaTrabalho/index.jsx
+++ src/componentes/secaoExperienciaTrabalho/index.jsx
@@ -21,14 +21,14 @@ export default function SecaoExperienciaTrabalho(props) {
           paragrafo="Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores"
         />
         <Card
-		ehTemaEscuro={props.ehTemaEscuro}
+		      ehTemaEscuro={props.ehTemaEscuro}
           data="AGOSTO 2016 - 2019"
           titulo="Product Designer"
           empresa="E Corp."
           paragrafo="Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra"
         />
         <Card
-		ehTemaEscuro={props.ehTemaEscuro}
+		      ehTemaEscuro={props.ehTemaEscuro}
           data="FEVEREIRO 2019 - 2021"
           titulo="Digital Consulting"
           empresa="Arasaka Inc."
